4a942e3df9af4a3a71704eb8461f604b3894db8b
Colin Ciervo <colinciervo@@gmail.com>
2023-02-02 17:43:29 -0500

    Add Main.js file

---
+++ src/components/Main/Main.js
@@ -0,0 +1,18 @@
+import './Main.css'
+import React, { Component } from 'react'
+
+class Main extends Component {
+    constructor() {
+        super()
+        this.state = {
+            
+        }
+    }
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
