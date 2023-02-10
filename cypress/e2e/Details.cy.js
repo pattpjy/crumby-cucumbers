b79fef58887aac3a9d3f317d28043b9a3cd73139
Colin Ciervo <colinciervo@@gmail.com>
2023-02-10 16:32:12 -0500

    refactor main test

--- cypress/e2e/Details.cy.js
+++ cypress/e2e/Details.cy.js
@@ -5,10 +5,6 @@ describe('Details page', () => {
             method: 'GET',
             url: 'https://rancid-tomatillos.herokuapp.com/api/v2/movies'
         }, { fixture: 'movies' })
-        cy.intercept({
-            method: 'GET',
-            url: 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270'
-        }, { fixture: 'details' })
     })
     it('Should have begin on the main page', () => {
         cy.get('.poster-display > :nth-child(1)')
@@ -16,6 +12,10 @@ describe('Details page', () => {
     it('should be able to go to a details page', () => {
         cy.get('.poster-display > :nth-child(1)')
             .click()
+            cy.intercept({
+            method: 'GET',
+            url: 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270'
+        }, { fixture: 'details' })
         cy.get('.poster')
         cy.get('.data-list > :nth-child(1)')
             .contains('Rating: 4')
