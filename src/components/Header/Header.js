b9a6adfaf4e197aba304f6513ac7f766378b9834
Colin Ciervo <colinciervo@@gmail.com>
2023-02-11 03:21:09 -0500

    add styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import "./Header.css";
 import { Link } from "react-router-dom";
+import homeGif from '../../assets/home.gif'
 
 const Header = () => {
   return (
     <div className="header" id="header">
       <Link to="/">
-        <h1 className="homeLink">CRUMBY CUCUMBERS</h1>
+        <img src={homeGif} className='gif'></img>
         </Link>
+        <h1 className="homeLink">CRUMBY CUCUMBERS</h1>
     </div>
   );
 };
