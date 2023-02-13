4561f57d781556fa030e8eebb747eb9ed8f83502
Patt Sookmark <pijiya@sookmark.com>
2023-02-13 10:25:33 -0700

    Add proptype to card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,22 @@
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 import "./Card.css";
 
 const Card = ({ poster, id, alt, handleClick }) => {
-
-    return (
-        <div className="main-board" onClick={handleClick}>
-            <Link to={{pathname: `/details/${id}`, state: { movieId: id }}}>
-                <img className="poster" src={poster} id={id} alt={alt}></img>
-            </Link>
-        </div>
-    );
+  return (
+    <div className="main-board" onClick={handleClick}>
+      <Link to={{ pathname: `/details/${id}`, state: { movieId: id } }}>
+        <img className="poster" src={poster} id={id} alt={alt}></img>
+      </Link>
+    </div>
+  );
 };
 
 export default Card;
+
+Card.propTypes = {
+  poster: PropTypes.string,
+  id: PropTypes.number,
+  alt: PropTypes.string,
+  handleClick: PropTypes.func,
+};
--- src/components/Search/SearchBar.js
+++ src/components/Search/SearchBar.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 const SearchBar = ({ searchMovies, clearSearchResult }) => {
   const [searchInput, setSearchInput] = useState("");
