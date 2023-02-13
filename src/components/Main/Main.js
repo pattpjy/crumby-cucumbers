5350c530a0d633ce301b5e67ffd63803091c3a0f
Patt Sookmark <pijiya@sookmark.com>
2023-02-12 21:29:50 -0700

    Add search bar and clear

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,14 @@ import "./Main.css";
 import React, { Component } from "react";
 // import movieData from "../../movieData";
 import Card from "../Card/Card";
+import SearchBar from "../Search/SearchBar";
 
 class Main extends Component {
   constructor() {
     super();
     this.state = {
       allMovies: [],
+      visibleMovies: [],
     };
   }
 
@@ -21,7 +23,7 @@ class Main extends Component {
       })
       .then((data) => {
         console.log(data.movies);
-        this.setState({ allMovies: data.movies });
+        this.setState({ allMovies: data.movies, visibleMovies: data.movies });
       })
       .catch((error) => this.setState({ hasError: error.message }));
   };
@@ -30,9 +32,11 @@ class Main extends Component {
     const Id = event.target.id;
     console.log(Id);
   };
-
+  clearSearchResult = () => {
+    this.setState({ visibleMovies: this.state.allMovies });
+  };
   displayMovies = () => {
-    const allMovies = this.state.allMovies.map((movie) => {
+    const allMovies = this.state.visibleMovies.map((movie) => {
       return (
         <Card
           poster={movie.poster_path}
@@ -46,17 +50,32 @@ class Main extends Component {
     return allMovies;
   };
 
+  displaySearch = (searchInput) => {
+    const searchMovies = this.state.allMovies.filter((movie) =>
+      movie.title.toLowerCase().includes(searchInput.toLowerCase())
+    );
+    this.setState({ visibleMovies: searchMovies });
+  };
+
   render() {
     return (
-      <div className="poster-display">
-        {this.state.hasError && (
-          <div className="submitErrorMessage">
-            <p>
-              <strong>{this.state.hasError}</strong>
-            </p>
-          </div>
-        )}
-        {this.displayMovies()}
+      <div>
+        <div className="search-bar">
+          <SearchBar
+            searchMovies={this.displaySearch}
+            clearSearchResult={this.clearSearchResult}
+          />
+        </div>
+        <div className="poster-display">
+          {this.state.hasError && (
+            <div className="submitErrorMessage">
+              <p>
+                <strong>{this.state.hasError}</strong>
+              </p>
+            </div>
+          )}
+          {this.displayMovies()}
+        </div>
       </div>
     );
   }
---
+++ src/components/Search/SearchBar.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const SearchBar = ({ searchMovies, clearSearchResult }) => {
+  const [searchInput, setSearchInput] = useState("");
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+  const submitSearch = (e) => {
+    e.preventDefault();
+    searchMovies(searchInput);
+    // if ({ searchInput } === "") {
+    //   setSearchInput({ incompleteForm: true });
+    // } else {
+    //   setSearchInput({ incompleteForm: false });
+    //   searchMovies({ searchInput });
+    // }
+  };
+  const clearSearch = (e) => {
+    setSearchInput("");
+    clearSearchResult();
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search here"
+        onChange={handleChange}
+        value={searchInput}
+      />
+      <button onClick={(event) => submitSearch(event)}>SUBMIT</button>
+      {/* {this.state.incompleteForm && (
+        <div className="incomplete-form">Please fill all the boxes</div>
+      )} */}
+      <button onClick={(event) => clearSearch(event)}>CLEAR</button>
+    </div>
+  );
+};
+
+export default SearchBar;
